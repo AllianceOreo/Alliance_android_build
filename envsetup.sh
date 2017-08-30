9861fd726cf297d97cd66e374dd65a0b0b87bb8c
sparx <alliancesparx@gmail.com>
2017-08-30 10:30:19 +0100

    Revert "build: Preliminary support for Custom product builds"
    This reverts commit e28c419e5dc8824985cf1ef31e92963800d88633.

--- envsetup.sh
+++ envsetup.sh
@@ -129,14 +129,6 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-
-    if (echo -n $1 | grep -q -e "^aosp_") ; then
-       CUSTOM_BUILD=
-    else
-       CUSTOM_BUILD=$1
-    fi
-    export CUSTOM_BUILD
-
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -607,7 +599,6 @@ function lunch()
         return 1
     fi
 
-    check_product $product
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
     TARGET_PLATFORM_VERSION=$version \
